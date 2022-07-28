0a3a360e91e74ff4948c20fac427d73703d7360a
serey_roth <roths7@wwu.edu>
2022-07-27 18:14:56 -0700

    Create DOM object getter file

---
+++ src/modules/ui.js
@@ -0,0 +1,6 @@
+export const addForm = document.getElementById('addForm');
+export const name = document.getElementById('pokemonName');
+export const gender = document.getElementsByClassName('pokemonGender');
+export const addBtn = document.getElementById('addBtn');
+
+export const team = document.getElementById('team');
