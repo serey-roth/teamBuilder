1501e1270bd77eea1498e4294a210fef88fd5237
serey_roth <roths7@wwu.edu>
2022-07-30 14:51:02 -0700

    Update ui.js

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,6 +1,6 @@
 export const addForm = document.getElementById('addForm');
-export const name = document.getElementById('pokemonName');
-export const gender = document.getElementsByClassName('pokemonGender');
+export const name = document.getElementById('personName');
+export const gender = document.getElementsByClassName('personGender');
 export const addBtn = document.getElementById('addBtn');
 
 export const team = document.getElementById('team');
